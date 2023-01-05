d0baae354c013f942dcd51aeff188aed8b53c2e6
Beatriz <byalt1228@gmail.com>
2023-01-05 19:38:05 -0300

    Desafio Concluido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -2,6 +2,8 @@ namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
     {
+        private const double V = 0.10;
+
         public List<Pessoa> Hospedes { get; set; }
         public Suite Suite { get; set; }
         public int DiasReservados { get; set; }
@@ -17,7 +19,8 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +28,7 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                 throw new Exception("NÚMERO DE HOSPEDES MAIOR QUE A CAPACIDADE DA SUÍTE");
             }
         }
 
@@ -37,7 +41,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
@@ -46,12 +50,15 @@ namespace DesafioProjetoHospedagem.Models
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
             decimal valor = 0;
+            valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal desconto = 0.10M ;
+                decimal calc = valor * desconto;
+                valor = valor - calc;
             }
 
             return valor;
